3ccebe98d316356c22ad5b7365406de8b27b668d
Angelica Mei De Villa <devilla.angelicamei@gmail.com>
2024-12-22 21:06:48 +0800

    Optimize the code

--- applications/views.py
+++ applications/views.py
@@ -42,7 +42,6 @@ def login_user(request):
                     request.session.set_expiry(3600 * 24 * 30)  # 30 days
                 else:
                     request.session.set_expiry(0)
-                # messages.success(request, 'Successfully logged in!')
                 return redirect('dashboard')
             else:
                 messages.error(request, 'Invalid username or password.')
@@ -50,7 +49,7 @@ def login_user(request):
         else:
             messages.error(request, 'Please correct the errors below.')
     else:
-        form = LoginForm() 
+        form = LoginForm()
     return render(request, 'registration/login.html', {'form': form})
 
 def logout_user(request):
@@ -77,13 +76,12 @@ def dashboard(request):
     progress_percentage = (applications_submitted / 20) * 100
     progress_percentage = int(progress_percentage)
 
-    monthly_applications = (
-        applications.annotate(month=ExtractMonth('date_applied'), year=ExtractYear('date_applied')).values('month', 'year').annotate(total=Count('id')).order_by('year', 'month'))
+    monthly_applications = (applications.annotate(month=ExtractMonth('date_applied'), year=ExtractYear('date_applied')).values('month', 'year').annotate(total=Count('id')).order_by('year', 'month'))
     labels = [datetime(year=data['year'], month=data['month'], day=1).strftime("%B %Y") for data in monthly_applications]
     totals = [data['total'] for data in monthly_applications]
 
-    upcoming_interviews = applications.filter(interview_schedule__gte=timezone.now()).order_by('interview_schedule')[:3]
-    reminders = Reminders.objects.filter(user=request.user)
+    # upcoming_interviews = applications.filter(interview_schedule__gte=timezone.now()).order_by('interview_schedule')[:3]
+    # reminders = Reminders.objects.filter(user=request.user)
     response_received = applications.filter(status__in=["Interviewing", "Offer Received", "Rejected"]).count()
     if total_applications > 0:
         response_rate = round((response_received / total_applications) * 100, 2)
@@ -105,22 +103,20 @@ def dashboard(request):
         'labels': labels,
         'totals': totals,
         'no_data': len(labels) == 0,
-        'upcoming_interviews' : upcoming_interviews,
-        'current_streak': current_streak,
+        # 'upcoming_interviews' : upcoming_interviews,
+        # 'current_streak': current_streak,
         'response_received' : response_received,
         'response_rate' : response_rate,
-        'reminders' : reminders,
+        # 'reminders' : reminders,
     }
     return render(request, 'dashboard.html', context)
 
 def applications(request):
     applications = JobApplication.objects.filter(user=request.user)
     search_query = request.GET.get('search', '')
-    
     if search_query:
         applications = applications.filter(company_name__icontains=search_query) | applications.filter(position__icontains=search_query)
     all_applications = applications.all()
-    
     if request.method == "POST":
         form = JobApplicationForm(request.POST)
         if form.is_valid():
@@ -132,7 +128,6 @@ def applications(request):
             return redirect('applications')
     else:
         form = JobApplicationForm()
-
     context = {
         'all_applications': all_applications,
         'form': form,
@@ -177,18 +172,16 @@ def reminders(request):
     return render(request, 'dashboard.html', context)
 
 def calendar(request):
-    # Get the current user's job applications and reminders
     user = request.user
     job_applications = JobApplication.objects.filter(user=user).exclude(interview_schedule__isnull=True)
     reminders = Reminders.objects.filter(user=user)
 
-    # Prepare data to pass to the template
     interview_data = []
     for job in job_applications:
         interview_data.append({
             'title': f"Interview for {job.position} at {job.company_name}",
-            'start': job.interview_schedule.strftime('%Y-%m-%dT%H:%M:%S'),  # Proper format
-            'end': (job.interview_schedule + timedelta(hours=1)).strftime('%Y-%m-%dT%H:%M:%S'),  # Assuming interview lasts 1 hour
+            'start': job.interview_schedule.strftime('%Y-%m-%dT%H:%M:%S'),
+            'end': (job.interview_schedule + timedelta(hours=1)).strftime('%Y-%m-%dT%H:%M:%S'),
             'description': job.location,
         })
 
@@ -197,17 +190,14 @@ def calendar(request):
         reminder_data.append({
             'title': reminder.title,
             'start': reminder.reminder_time.strftime('%Y-%m-%dT%H:%M:%S'),  # Proper format
-            'end': (reminder.reminder_time + timedelta(hours=1)).strftime('%Y-%m-%dT%H:%M:%S'),  # Assuming reminder lasts 1 hour
+            'end': (reminder.reminder_time + timedelta(hours=1)).strftime('%Y-%m-%dT%H:%M:%S'),
             'description': reminder.description,
         })
-
-    # Combine both interview and reminder data for the calendar
     events = interview_data + reminder_data
 
     return render(request, 'calendar.html', {'events': events})
 
 def settings(request):
-    
     return render(request, 'settings.html')
 
 @login_required
@@ -216,17 +206,14 @@ def edit_profile(request):
     if request.method == 'POST':
         form = UserProfileForm(request.POST, request.FILES, instance=user_profile)
         form2 = UserChangeForm(request.POST, instance=request.user)
-
         if form.is_valid():
-            form.save()  # Save profile changes (avatar, etc.)
+            form.save()
         else:
-            print("UserProfileForm Errors:", form.errors)  # Debugging form errors
-
+            print("UserProfileForm Errors:", form.errors)
         if form2.is_valid():
-            form2.save()  # Save user details (first_name, last_name, etc.)
+            form2.save()
         else:
-            print("UserChangeForm Errors:", form2.errors)  # Debugging form errors
-
+            print("UserChangeForm Errors:", form2.errors)
         messages.success(request, 'Your profile has been updated successfully!')
         return redirect('settings')
     else:
@@ -244,9 +231,9 @@ def change_password(request):
         form = PasswordChangeForm(user=request.user, data=request.POST)
         if form.is_valid():
             form.save()
-            update_session_auth_hash(request, form.user)  # Keep user logged in
+            update_session_auth_hash(request, form.user)
             messages.success(request, "Your password has been successfully changed!")
-            return redirect('settings')  # Redirect to settings page or any other desired page
+            return redirect('settings')
         else:
             messages.error(request, "Please correct the errors below.")
     return render(request, 'settings.html', {'form': form})
@@ -254,12 +241,7 @@ def change_password(request):
 @login_required
 def delete_account(request):
     if request.method == 'POST':
-        # Delete the user account
         user = request.user
         user.delete()
-        # Log the user out after deletion
         logout(request)
-        # # Redirect to a page confirming account deletion
-        # return redirect('account_deleted')
-    # If not a POST request, show a confirmation page (Optional)
     return render(request, 'delete_account_confirm.html')
